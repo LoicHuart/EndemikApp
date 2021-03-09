3fea114219204e6a117908d24ab5c902bc8352cd
LHuartNisgroup <l.huart@nis-group.fr>
2021-03-09 19:39:34 +0100

    ajout constant color

---
+++ constants/color.js
@@ -0,0 +1,15 @@
+export default {
+    COLORS: {
+      DEFAULT: '#F7FAFC',
+      PRIMARY: '#C95658',
+      SECONDARY: '#ADD8E6',
+     
+      
+      SUCCESS: '#15D636',
+      WARNING: '#FFB300',
+      DANGER: '#FC4545',
+
+      WHITE: '#FFFFFF',
+      BLACK: '#000000'
+    }
+  };
