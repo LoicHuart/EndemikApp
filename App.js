fbd01e45ef662f6bb31e431cacc70d82c44d09c6
LHuartNisgroup <l.huart@nis-group.fr>
2021-03-09 16:23:57 +0100

    app base

--- App.js
+++ App.js
@@ -3,11 +3,7 @@ import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 
-<<<<<<< HEAD
-import { home, TestScreen } from './screens/';
-=======
 import { HomeScreen, NotificationsScreen } from './screens/';
->>>>>>> parent of 312d3d4 (testScreen)
 
 
 
@@ -17,15 +13,9 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-<<<<<<< HEAD
-      <Drawer.Navigator initialRouteName="home">
-        <Drawer.Screen name="home" component={home} />
-        <Drawer.Screen name="TestScreen" component={TestScreen} />
-=======
       <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="Notifications" component={NotificationsScreen} />
->>>>>>> parent of 312d3d4 (testScreen)
       </Drawer.Navigator>
     </NavigationContainer>
   );
--- screens/index.js
+++ screens/index.js
@@ -1,10 +1,2 @@
-<<<<<<< HEAD
-
-export { TestScreen } from './TestScreen';
-export { account } from "./account";
-export { home } from "./home";
-export { login } from "./login";
-=======
 export { HomeScreen } from './HomeScreen';
 export { NotificationsScreen } from './NotificationsScreen';
->>>>>>> parent of 312d3d4 (testScreen)
