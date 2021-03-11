11368b8b1f8263e51bdad9d05388208c6548791e
gaspard <tapongaspard16@gmail.com>
2021-03-11 23:03:33 +0100

    formulaire demande de congé mais uniquementvisuel le formik ne récupère pas les datas sauf le champ  note

--- constants/color.js
+++ constants/color.js
@@ -4,6 +4,7 @@ export default {
     PRIMARY: "#C95658",
     PRIMARYFADE: "rgba(201, 86, 88, 0.001)",
     SECONDARY: "#ADD8E6",
+    GREY: "#808080",
 
     SUCCESS: "#15D636",
     WARNING: "#FFB300",
--- screens/holidaysRequest/holidaysRequest.js
+++ screens/holidaysRequest/holidaysRequest.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
-import { HeaderCustom } from "../../components/";
+import { HeaderCustom, FormHolidaysRequest } from "../../components/";
 export const holidaysRequest = ({ navigation }) => {
   return (
     <View style={{ flex: 1 }}>
       <HeaderCustom navigation={navigation} title="HOLIDAYS REQUEST" />
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>holidaysRequest</Text>
-        <Button onPress={() => navigation.goBack()} title="Go Back" />
-      </View>
+      <FormHolidaysRequest />
     </View>
   );
 };
