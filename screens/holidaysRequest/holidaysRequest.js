ee41697ca24cd69579de39897f691d5c4d9ae844
gaspard-tapon <75847134+gaspard-tapon@users.noreply.github.com>
2021-03-11 23:09:56 +0100

    Revert "formulaire demande de congé mais uniquementvisuel le formik ne récupè…"

--- screens/holidaysRequest/holidaysRequest.js
+++ screens/holidaysRequest/holidaysRequest.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
-import { HeaderCustom, FormHolidaysRequest } from "../../components/";
+import { HeaderCustom } from "../../components/";
 export const holidaysRequest = ({ navigation }) => {
   return (
     <View style={{ flex: 1 }}>
       <HeaderCustom navigation={navigation} title="HOLIDAYS REQUEST" />
-      <FormHolidaysRequest />
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>holidaysRequest</Text>
+        <Button onPress={() => navigation.goBack()} title="Go Back" />
+      </View>
     </View>
   );
 };
