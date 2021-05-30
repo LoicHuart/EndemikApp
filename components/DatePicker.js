ef88aaa62ac886e89741ccd4abb5f32d8f571d8d
LHuartNisgroup <l.huart@nis-group.fr>
2021-05-30 18:54:56 +0200

    addEmployee, correction bug date iphone

--- components/DatePicker.js
+++ components/DatePicker.js
@@ -28,7 +28,7 @@ export const DatePicker = ({ onChange, value, errorMessage }) => {
                     <View style={{ justifyContent: "center", marginVertical: 3 }}>
                         <DateTimePicker
                             testID="dateTimePickerDateBirth"
-                            value={value}
+                            value={value && formatDisplay(value)}
                             locale="fr-FR"
                             mode="date"
                             display="default"
