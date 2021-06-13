2355cbe16f60149308d453b95cb2bfef7e7187e1
LHuartNisgroup <l.huart@nis-group.fr>
2021-06-13 17:41:09 +0200

    dashboard

--- screens/dashbord/dashbord.js
+++ screens/dashbord/dashbord.js
@@ -1,12 +1,9 @@
 import React, { useState, useContext, useEffect } from "react";
-import { StyleSheet, Text, View, FlatList,ScrollView } from "react-native";
-import { Button, Icon } from "react-native-elements";
-import { dashbaord } from "../../styles/dashbaord";
+import { Text, View } from "react-native";
+import { Icon } from "react-native-elements";
 import { HeaderCustom, ListHolidays } from "../../components/";
 import { Card } from "../../components/Card";
-import color from "../../constants/color";
-
-
+import { getHolidaysByUserApi } from "../../requestApi/";
 import { AuthContext } from "../../context/AuthContext";
 
 export const dashbord = ({ navigation, gestion }) => {
@@ -15,132 +12,57 @@ export const dashbord = ({ navigation, gestion }) => {
   const [hollidayWaiting, setHollidayWaiting] = useState();
   const [hollidayAccept, setHollidayAccept] = useState();
 
+  useEffect(() => {
+    const Pendingholliday = async () => {
+      await getHolidaysByUserApi(token, user._id, ["en attente", "prévalidé"])
+        .then((result) => setHollidayWaiting(result.length))
 
-  const Pendingholliday = async () => {
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", `Bearer ${token}`);
-
-    var requestOptions = {
-      method: "GET",
-      headers: myHeaders,
-      redirect: "follow",
+      await getHolidaysByUserApi(token, user._id, ["validé"])
+        .then((result) => setHollidayAccept(result.length))
     };
-    try {
-      const resp = await fetch(
-        `http://${process.env.REACT_APP_API_HOST}/api/holidays/user/${user._id}`,
-        requestOptions
-      );
-      const respJSON = await resp.json();
-      
-
-      if (!resp.ok) {
-        console.log("not ok");
-        console.log(resp);
-      }
-      setHollidays(respJSON);
-
-      let allWaiting = 0
-      let allAccept = 0
-      hollidays.forEach(holliday => {
-        if(holliday.status == "en attente"){
-          allWaiting += 1 
-        }
-        if(holliday.status == "prevalidé" || holliday.status == "validé"){
-          allAccept += 1 
-        }
-      });
-      setHollidayWaiting(allWaiting)
-      setHollidayAccept(allAccept)
-
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-
-  useEffect(( ) => {
     Pendingholliday()
-  }, [])
+  }, [token])
 
   return (
-    
+
     <View style={{ flex: 1 }}>
       <HeaderCustom navigation={navigation} />
-      <ScrollView>
-      <View style={{flexDirection: "row",    alignItems: "center", justifyContent: "center" }}>
-            <Card >
-              <Text style={{fontSize:11,align: "left"}}>Demande de congés en attente</Text>   
-              <View style={{flexDirection: "row",  alignItems: "center", justifyContent: "center"}}>
-                <Text style={{fontSize:50}}>{hollidayWaiting}</Text>
-                  <View style={{borderRadius:100, backgroundColor:"orange", padding:15, margin:10}} >
-                    <Icon
-                        name="hourglass-half"
-                        type="font-awesome-5"
-                        color="white"
-                        style={{ width:30}}
-                      />
-                  </View> 
-              </View> 
-            </Card>
-          <Card>
-            <Text style={{fontSize:11,align: "left"}}>Demande de congés traitées</Text>   
-            <View style={{flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
-              <Text style={{fontSize:50}}>{hollidayAccept}</Text>
-                <View style={{borderRadius:100, backgroundColor:"limegreen", padding:15, margin:10}} >
-                  <Icon
-                      name="check"
-                      type="font-awesome-5"
-                      color="white"
-                      style={{ width:30}}
-                    />
-                </View>
+      <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+        <Card >
+          <Text style={{ fontSize: 11 }}>Demande de congés en attente</Text>
+          <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+            <Text style={{ fontSize: 50 }}>{hollidayWaiting}</Text>
+            <View style={{ borderRadius: 100, backgroundColor: "orange", padding: 15, margin: 10 }} >
+              <Icon
+                name="hourglass-half"
+                type="font-awesome-5"
+                color="white"
+                style={{ width: 30 }}
+              />
             </View>
-          </Card>
+          </View>
+        </Card>
+        <Card>
+          <Text style={{ fontSize: 11 }}>Demande de congés traitées</Text>
+          <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+            <Text style={{ fontSize: 50 }}>{hollidayAccept}</Text>
+            <View style={{ borderRadius: 100, backgroundColor: "limegreen", padding: 15, margin: 10 }} >
+              <Icon
+                name="check"
+                type="font-awesome-5"
+                color="white"
+                style={{ width: 30 }}
+              />
+            </View>
+          </View>
+        </Card>
       </View>
-      <Card >
-          <ListHolidays third={true} token={token} user={user} noTouch={true} status={["en attente", "prévalidé", "annulé", "validé", "refusé"]}/>
-      </Card>
-      <Button onPress={signOut} title='Logout'/>
-      </ScrollView>
+      <View style={{ flex: 1 }}>
+        <Card >
+          <ListHolidays third={true} token={token} user={user} noTouch={true} status={["en attente", "prévalidé", "annulé", "validé", "refusé"]} />
+        </Card>
+      </View>
     </View>
-    
+
   );
 };
-
-const styles = StyleSheet.create({
-});
-
-            {/* <FlatList
-            data={hollidays}
-            ListEmptyComponent={() => (
-              <View style={{ alignItems: "center" }}>
-                <Text style={{ margin: 10 }}>
-                  Vous n'avez pas encore de demandes
-                </Text>
-                <Text style={{ color: color.COLORS.GREY }}>
-                  Balayez vers le bas pour actualiser
-                </Text>
-                <Icon
-                  name="angle-double-down"
-                  type="font-awesome-5"
-                  color={color.COLORS.GREY}
-                />
-              </View>
-            )}
-            refreshing={loading}
-            onRefresh={() => displayHolidays()}
-            renderItem={({ item }) => (
-              <CardHoliday
-              item={item}
-              gestion={gestion}
-              refreshHolidays={Pendingholliday}
-            />
-             )}
-            keyExtractor={(item) => item._id}
-          />
-          <Icon
-            name="plus-circle"
-            type="font-awesome-5"
-            color="grey"
-            style={{ width:30, marginBottom:20}}
-          /> */}
